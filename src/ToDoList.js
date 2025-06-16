6dc54462c75148babb9bc213d47889a62c93ae18
Anastasia <v4silieva.an45@yandex.ru>
2025-06-16 19:48:37 +0300

    new styles

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -7,32 +7,35 @@ import ToDoTask from './ToDoTask';
 class ToDoList extends React.Component {
   render() {
     return (
-
-		<div classNameName="card-hover-shadow-2x mb-3 card">
+      
+  <div className="card-hover-shadow-2x mb-3 card">
         <div className="card-header-tab card-header">
           <div className="card-header-title font-size-lg text-capitalize font-weight-normal"><i
               className="fa fa-tasks"></i> &nbsp;Task Lists</div>
           
         </div>
         <div className="scroll-area-sm">
+          <perfect-scrollbar className="ps-show-limits">
             <div style={{position: "static"}} className="ps ps--active-y">
               <div className="ps-content">
                 <ul className=" list-group list-group-flush">
-                  {
-					this.props.tasks.map((task) => {
-						return (
-							<ToDoTask task={task} key={task._id} />
-						);
-					})
-				}  
+                {
+            this.props.tasks.map((task) => {
+              return (
+                <ToDoTask task={task} key={task._id} />
+              );
+            })
+          }  
                 </ul>
               </div>
               
             </div>
+          </perfect-scrollbar>
         </div>
         <div className="d-block text-right card-footer">
-<NavLink to="/add" className="btn btn-primary" >Add task</NavLink></div>
+            <NavLink to="/add" className="btn btn-primary">Add task</NavLink></div>
       </div>
+      
     );
   }
 }
