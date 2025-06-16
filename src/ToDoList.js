1b58883c9d0227601074288b1622f6a493fa4a6a
Anastasia <v4silieva.an45@yandex.ru>
2025-06-16 11:18:23 +0300

    Apply custom UI template

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,25 +1,46 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import ToDoTask from './ToDoTask';
-import  { useNavigate }  from 'react-router-dom';
 
 class ToDoList extends React.Component {
-
   render() {
+    return (
 
-    return (
-      <div className="List">
-  
-        <ul>
-        {
-	this.props.tasks.map((task) => {
-    return (
-     <ToDoTask task={task} onTaskDelete={this.props.onTaskDelete}  key={task._id} />
-    )
-   })
-  }
-        </ul>
+		<div classNameName="card-hover-shadow-2x mb-3 card">
+        <div className="card-header-tab card-header">
+          <div className="card-header-title font-size-lg text-capitalize font-weight-normal"><i
+              className="fa fa-tasks"></i> &nbsp;Task Lists</div>
+          
+        </div>
+        <div className="scroll-area-sm">
+            <div style={{position: "static"}} className="ps ps--active-y">
+              <div className="ps-content">
+                <ul className=" list-group list-group-flush">
+                  {
+					this.props.tasks.map((task) => {
+						return (
+							<ToDoTask task={task} key={task._id} />
+						);
+					})
+				}  
+                </ul>
+              </div>
+              
+            </div>
+        </div>
+        <div className="d-block text-right card-footer">
+<NavLink to="/add" className="btn btn-primary" >Add task</NavLink></div>
       </div>
     );
   }
 }
-export default ToDoList;
+function mapStateToProps(state) {
+  return {
+    tasks: [...state.tasks]
+  };
+}
+
+
+export default connect(mapStateToProps)(ToDoList);
