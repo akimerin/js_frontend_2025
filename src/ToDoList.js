2881e541f423597d2a6c4e57ebc011708af30f74
akimerin <146888622+akimerin@users.noreply.github.com>
2025-06-17 11:00:53 +0300

    Update ToDoList.js

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -8,7 +8,7 @@ class ToDoList extends React.Component {
   render() {
     return (
 
-		<div classNameName="card-hover-shadow-2x mb-3 card">
+	<div className="card-hover-shadow-2x mb-3 card">
         <div className="card-header-tab card-header">
           <div className="card-header-title font-size-lg text-capitalize font-weight-normal"><i
               className="fa fa-tasks"></i> &nbsp;Task Lists</div>
